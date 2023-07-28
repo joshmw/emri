442c5ee0e433d5f3b4aef73eeb04cebc4158c8fb
Josh Wilson <joshmw@stanford.edu>
2023-07-28 16:06:29 -0700

    show sqrtn scan averaging improvement. used to justify analyses in getStdOfRealData and frequencyApproachSNR

--- snrSimulAnal.m
+++ snrSimulAnal.m
@@ -1,5 +1,9 @@
 function snrSimulAnal
 
+showVoxelAveraging = 0;
+showMeanAveraging = 1;
+
+
 %go to data directory
 dataDir = '/Users/joshwilson/Library/CloudStorage/OneDrive-Stanford/emri/oldEmriSimulationData/simulData/pinkNoiseSweep';
 cd(dataDir)
@@ -12,6 +16,7 @@ brain = (round(params.xdim/2)-params.brainSize/2):(round(params.xdim/2)+params.b
 ts = squeeze(ts);
 noiselessTimeSeries = squeeze(mean(ts(brain,brain,:),[1 2]));
 
+if showVoxelAveraging
 %% compare at different noise levels
 figure(115), sub = 1;
 for seed = 100
@@ -46,6 +51,7 @@ for seed = 100
             shuffledNoisyTimeSeries = noisyTimeSeries(randperm(length(noisyTimeSeries)));
             permutedCorVals = [permutedCorVals corr2(noiselessTimeSeries,shuffledNoisyTimeSeries)];
         end
+        %sort the permuted values and get upper/lower bounds
         permutedCorVals = sort(permutedCorVals);
         permutedCorValsHigh = [permutedCorValsHigh permutedCorVals(475)];
         permutedCorValsLow = [permutedCorValsLow permutedCorVals(25)];
@@ -65,42 +71,59 @@ figure,scatter([1:40]/40,noiseStds)
 xlabel('Std of injected pink noise in simulation')
 ylabel('Std of averaged voxel time series (noise part)')
 
+end
 
-%% plot different number of averages for noisiest condition
-sub = 1;
-avgts = zeros(1,200);
-noiseStdsAveraged = [];
-for seed = 100:124
-    for pinkNoiseStd = 1;
+
+
+%% show the effects of averaging on data simulated to look like human data
+%go to data
+dataDir = '/Users/joshwilson/Library/CloudStorage/OneDrive-Stanford/emri/oldEmriSimulationData/simulData/snrSimul';
+cd(dataDir)
+
+colors{1} = [0 208 233]/255;
+colors{2} = [126 200 42]/255;
+pinkNoiseStd = [[1.07 1.4]];
+
+for noiseLevel = 1:length(pinkNoiseStd)
+    avgts = zeros(1,200);
+    noiseStdsAveraged = [];
+    for seed = 1:200
         %load the file and squeeze+average the timeseries over the brain
-        fileName = sprintf('seed%4.4gpinkNoise%4.4g',seed,pinkNoiseStd); fileName = replace(fileName, '.', '_'); fileName = replace(fileName, ' ', '');
+        fileName = sprintf('seed%4.4gpinkNoise%4.4g',seed,pinkNoiseStd(noiseLevel)); fileName = replace(fileName, '.', '_'); fileName = replace(fileName, ' ', '');
         load(fileName);
         noisyTimeSeries = squeeze(ts);
         noisyTimeSeries = squeeze(mean(noisyTimeSeries(brain,brain,:),[1 2]));
         %average the different runs and plot
-        avgts = squeeze(avgts) + squeeze(noisyTimeSeries')./25;
-        noiseStdsAveraged = [noiseStdsAveraged std(avgts(50:200).*(25/(seed-99)))];
-        figure(118)
-        subplot(5,5,sub)
-        plot(avgts.*(25/(seed-99))), ylim([99 102]);
-        sub = sub+1;
-        %plot the correlation
-        figure(119), hold on,
-        scatter(seed-99,std(avgts(50:200).*(25/(seed-99)))),
-        xlabel('Number of scans averaged'),
-        ylabel('SNR')
+        avgts = squeeze(avgts) + squeeze(noisyTimeSeries')./200;
+        noiseStdsAveraged = [noiseStdsAveraged std(avgts(50:200).*(200/(seed)))];
+        %plot if
+        if noiseLevel == 1;
+            if (seed == 12) | (seed == 47) | (seed == 106) | (seed == 188)
+                figure
+                plot(avgts.*(200/(seed))), ylim([99.8 100.5]);
+            end
+        end
     end
+    figure(125); hold on
+    scatter(1:200,noiseStdsAveraged,'filled','MarkerFaceColor',colors{noiseLevel},'MarkerEdgeColor','w'),   
+    title('Influence of averaging over scans on noise magnitude')
+    xlabel('Number of scans averaged')
+    ylabel('Std of time series outside of signal')
+    %fit
+    oneOverSqrtN = fittype('a/(sqrt(x))', 'independent', 'x', 'dependent', 'y' );
+    opts = fitoptions( 'Method', 'NonlinearLeastSquares' ); opts.Display = 'Off'; opts.StartPoint = [0];
+    oneOverSqrtNFit = fit([1:length(noiseStdsAveraged)]',noiseStdsAveraged',oneOverSqrtN,opts);
+    plot(oneOverSqrtNFit)
+    xlim([0 100]); ylim([0 .75])
 end
-xlabel('Number of averaged time series'), ylabel('Correlation with noiseless signal')
+plot([0 100], [.169/2 .169/2])
 
 keyboard
 
 
-
-
-
-
-
+%%%%%%%%%%%%%
+%% helpers %%
+%%%%%%%%%%%%%
 
 function labelGraphs
 figure(115),subplot(5,8,17),
@@ -113,59 +136,6 @@ set(gca,'YTickLabel',[-.3 1]), ylabel('% correlation')
 subplot(5,8,36), 
 set(gca,'XTickLabel',[-200 200]), xlabel('Lag (ms)')
 
-
-
-
-
-
-
-function getStdOfRealData
-
-cd ~/data/emri/paperdata
-SIDs = ["s0615" "s0616" "s0617" "s0618" "s0619"];
-
-allVoxelStds = [];
-avgStds = [];
-for subject = 1:length(SIDs);
-
-    cd(SIDs(subject));
-    %create the view and go to unaveraged ungated scana
-    v = newView;
-    v = viewSet(v,'curGroup','rawUngated')
-    v = loadROI(v,'insideBrain') ;
-    numScans = viewGet(v,'numScans');
-    %loop through each scan
-    for scan = 1:numScans
-        %set the current scan
-        v = viewSet(v,'curScan',scan)    
-        %load the t series
-        roi = loadROITSeries(v,'insideBrain',viewGet(v,'curScan'),viewGet(v,'curGroup'));
-        
-        
-
-        %invidiaul t series std
-        alltSeriesMeans = mean(roi.tSeries,2);
-        alltSeries = 100*(roi.tSeries-alltSeriesMeans)./repmat(alltSeriesMeans,[1 size(roi.tSeries,2)]);
-        allVoxelStds = [allVoxelStds std(alltSeries,0,2)'];
-
-        tSeries = roi.tSeries
-        %average across all voxels
-        tSeries = mean(tSeries,1)';
-        %convert to % signal change
-        meanTSeries = mean(tSeries);
-        tSeries = 100*(tSeries-meanTSeries)/meanTSeries;
-    
-        %save the std of the averaged voxels in this scan
-        avgStds = [avgStds std(tSeries)];
-
-    end
-    %go back and do it over
-    pause(1)
-    deleteView(v)
-    mrQuit
-    cd ..
-   
-end
 
 
 function checkAverageNoiseReduction(roi)
